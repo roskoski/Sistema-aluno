d58ebec9674bda2ea6db15a86129ac65774a4e73
Eduardo Roskoski <eduardo.roskoski.oliveira@escola.pr.gov.br>
2026-03-04 10:51:14 -0300

    index

---
+++ script.js
@@ -0,0 +1,21 @@
+function calcularNota() {
+
+    //const -> valor fixo
+    const mediaMinima =7;
+
+    //let -> valores que podem mudar
+    let nome = prompt("Digite o nome do aluno:");
+
+    let nota1 = Number(prompt("Digite a primeira nota:"));
+    let nota2 = Number(prompt("Digite a segunda nota:"));
+    let nota3 = Number(prompt("Digite a terceira nota:"));
+
+    //operadores aritméticos
+    let soma = nota1 + nota2 + nota3;
+    let media = soma / 3;
+
+    //operadores relacionais + lógicos
+
+    let aprovado = media >= mediaMinima;
+    let recuperacao = media >= 5 && media < 7;
+    let reprovado = media < 5;
